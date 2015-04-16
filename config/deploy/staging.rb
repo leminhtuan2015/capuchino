43942eadd687d1fea991aa2bd32a8d69386176d9
leminhtuan2015 <leminhtuan2015@gmail.com>
2015-04-16 22:27:10 +0700

    v2

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -49,13 +49,9 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+server '133.242.183.87',
+  user: 'framgia',
+  roles: %w{web app},
+  ssh_options: {
+    # password: 'please use keys'
+  }
