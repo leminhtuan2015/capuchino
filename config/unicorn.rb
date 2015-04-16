e999acc5cb51fea0ff584e77d9f9495ab7e8573b
leminhtuan2015 <leminhtuan2015@gmail.com>
2015-04-17 00:31:12 +0700

    v11

---
+++ config/unicorn.rb
@@ -0,0 +1,23 @@
+# working_directory "/path/to/your/app"
+working_directory "/home/framgia/capuchino"
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "/home/framgia/capuchino/pids/unicorn.pid"
+
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/var/www/my_app/log/unicorn.log"
+stdout_path "/var/www/my_app/log/unicorn.log"
+
+# Unicorn socket
+listen "/tmp/unicorn.capuchino.sock"
+listen "/tmp/unicorn.myapp.sock"
+
+# Number of processes
+# worker_processes 4
+worker_processes 2
+
+# Time-out
+timeout 30
