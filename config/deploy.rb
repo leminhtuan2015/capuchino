791564662038db617848c0db29dc3416ef2211f2
leminhtuan2015 <leminhtuan2015@gmail.com>
2015-04-17 00:08:46 +0700

    v10

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,29 +10,18 @@ set :repo_url, 'git@github.com:leminhtuan2015/capuchino.git'
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/framgia/capuchino'
 
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :pty, true
+set :sudo_prompt, ""
+set :linked_files, %w{
+  config/database.yml
+  config/nginx.production.conf
+  config/secrets.yml
+  config/unicorn.rb
+  config/unicorn_init.sh
+}
+set :linked_dirs, %w{ tmp log }
+set :scm, :git
+set :tmp_dir, "/home/#{fetch(:application)}/tmp"
 
 namespace :deploy do
 
